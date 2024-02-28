1cb8da802ecc3b18bbccd45e7abf3352a87a4d5d
Crowdbotics <team@crowdbotics.com>
2024-02-28 23:25:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-dew-46710",
+  url: "https://orange-dew-46710.botics.co",
+  api: "https://orange-dew-46710.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
